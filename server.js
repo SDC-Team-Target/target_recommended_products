38f58040699eda8b84c385f835a5941cfd6296fc
Mariella-Arias <ariasmariella.brm@gmail.com>
2020-08-07 17:58:06 -0500

    Implements a kitchen get route and sends correct data as response.

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const path = require('path');
+const {queryKitchenProducts} = require('./db/index');
 const PORT = 4040;
 
 const app = express();
 
 app.use(express.static('./public'));
+
+app.use('/kitchen', (req, res) => {
+  queryKitchenProducts((err, items)=> {
+    if (err) {
+      res.status(500).send('Could not retrieve items');
+    } else {
+      res.status(200).send(items);
+    }
+  })
+})
+
 
 app.listen(PORT, ()=> console.log(`Server listening on port: ${PORT}`));
