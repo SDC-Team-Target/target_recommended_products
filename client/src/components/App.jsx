66ca4fa451ad7fd9c2d7e1190b832efb70aec8cc
Mariella-Arias <ariasmariella.brm@gmail.com>
2020-08-07 11:47:10 -0500

    Renders Tabs with string content.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,45 @@
 import React, {Component} from 'react';
+import { Tab, Tabs } from 'react-bootstrap';
+import Tab1 from './Tab1.jsx';
+import Tab2 from './Tab2.jsx';
+import Tab3 from './Tab3.jsx';
 
 export default class App extends Component {
   constructor(props){
     super();
     this.state = {
-      initial: 'Hello from react App'
+      activeTab: 'More to consider',
+      key: 'home'
     }
+    this.handleTabClick = this.handleTabClick.bind(this);
+  }
+
+  handleTabClick(tab) {
+    this.setState({activeTab: tab});
   }
 
   render() {
-    const{initial} = this.state;
+    const{initial, key} = this.state;
+    
     return(
-    <div>{initial}</div>
+        <Tabs
+        id="controlled-tab-example"
+        transition={false} 
+        activeKey={key}
+        onSelect={key => this.setState({ key })}
+      >
+        <Tab eventKey="more to consider" title="More to consider">
+            more to consider data
+    
+        </Tab>
+        <Tab eventKey="similar items" title="Similar items">
+            similar items
+
+        </Tab>
+        <Tab eventKey="guests also bought" title="Guests also bought">
+        guests also bought
+        </Tab>
+      </Tabs>
     )
   }
-}
+};
